bdfd62fd74272316c060c050f6a33999056ee7e8
shengyeong <chia.shengyeong@gmail.com>
2014-10-07 18:00:35 +0800

    commiting after building up forms

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by username: params[:username]
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      flash[:notice] = "welcome, you're logged in!"
+      redirect_to user_path(user.username)
+    else
+      flash.now[:error] = "There is something wrong with your username or password." # flash says, show this message on the next redirect. flash.now as show it now.
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "you've logged out."
+    redirect_to login_path
+  end
+
+end
---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,28 @@
+class StatusesController < ApplicationController
+
+  before_action :require_user, only: [:new, :create]
+
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = Status.new(status_params)
+    user = User.find 1
+    @status.creator = user #TODO: update this after authentication
+
+    if @status.save
+      flash[:notice] = "Status created"
+      redirect_to user_path(user.username)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def status_params
+    params.require(:status).permit(:body)
+  end
+end
